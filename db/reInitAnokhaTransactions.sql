17b5cdd2f16f3bc82482317e1c5c52b849f50d4c
Ashrockzzz2003 <ashrockzzz2003@gmail.com>
2024-01-15 08:33:13 +0530

    transactionTable schema

---
+++ db/reInitAnokhaTransactions.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS transactionData;
+
+CREATE TABLE IF NOT EXISTS transactionData (
+    txnid VARCHAR(255) NOT NULL,
+    userId INT NOT NULL,
+    amount INT NOT NULL,
+    productinfo VARCHAR(5000) NOT NULL,
+    firstname VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    phone VARCHAR(10) NOT NULL,
+    transactionStatus VARCHAR(1) NOT NULL,
+    createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    lastUpdatedAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (txnid),
+    CHECK (transactionStatus IN ('0', '1', '2'))
+    -- 0: Pending, 1: Success, 2: Failed
+);
