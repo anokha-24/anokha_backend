3edbc006668aaa27b80700a8b03d3eef4d5a32b7
Abhinav R <76396917+Abhinav-ark@users.noreply.github.com>
2024-01-17 04:48:48 +0530

    new user routes

--- router/userRouter.js
+++ router/userRouter.js
@@ -4,5 +4,8 @@ const express = require('express');
 const userRouter = express.Router();
 
 userRouter.get('/test', userController.testConnection);
+userRouter.get('/getAllEvents',userController.getAllEvents);
+userRouter.get('/getRegisteredEvents',userController.getRegisteredEvents);
+userRouter.get('/getStarredEvents',userController.getStarredEvents);
 
 module.exports = userRouter;
