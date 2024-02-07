3d290debfbcde862344705e735c17b618474babc
Abhinav R <76396917+Abhinav-ark@users.noreply.github.com>
2024-02-07 09:32:42 +0530

    redis cache set -> set + expire for 10mins only

--- controller/userController.js
+++ controller/userController.js
@@ -1153,7 +1153,8 @@ module.exports = {
                                 const result = Array.from(aggregatedDataMap.values());
 
                                 //console.log(result);
-                                await redisClient.setex('allEvents',600, JSON.stringify(result));
+                                await redisClient.set('allEvents', JSON.stringify(result));
+                                await redisClient.expire('allEvents', 600);
                                 //await redisClient.disconnect()
 
                                 // MODE 0 - Not Logged In
