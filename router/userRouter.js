471605726f57788fdf41ce9d5d9a742459c840c7
Abhinav R <76396917+Abhinav-ark@users.noreply.github.com>
2024-02-12 01:30:12 +0530

    production routes

--- router/userRouter.js
+++ router/userRouter.js
@@ -3,15 +3,15 @@ const userController = require('../controller/userController');
 const express = require('express');
 const userRouter = express.Router();
 
-userRouter.get('/test', userController.testConnection);
-userRouter.get('/getAllEvents', userController.getAllEvents);
-userRouter.get('/getRegisteredEvents', userController.getRegisteredEvents);
-userRouter.get('/getStarredEvents', userController.getStarredEvents);
-userRouter.post('/toggleStarredEvent', userController.toggleStarredEvent);
+//userRouter.get('/test', userController.testConnection);
+//userRouter.get('/getAllEvents', userController.getAllEvents);
+//userRouter.get('/getRegisteredEvents', userController.getRegisteredEvents);
+//userRouter.get('/getStarredEvents', userController.getStarredEvents);
+//userRouter.post('/toggleStarredEvent', userController.toggleStarredEvent);
 userRouter.get('/getStudentProfile', userController.getStudentProfile);
 userRouter.post('/editStudentProfile', userController.editStudentProfile);
-userRouter.post('/registeredEventData', userController.registeredEventData);
-userRouter.get('/getEventData/:eventId(\\d+)', userController.getEventData);
-userRouter.post('/registerForEventStepOne', userController.registerForEventStepOne);
+//userRouter.post('/registeredEventData', userController.registeredEventData);
+//userRouter.get('/getEventData/:eventId(\\d+)', userController.getEventData);
+//userRouter.post('/registerForEventStepOne', userController.registerForEventStepOne);
 
 module.exports = userRouter;
