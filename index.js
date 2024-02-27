c856d3f66908d6723e0ec6eccfb3ee64404dab15
Abhinav R <76396917+Abhinav-ark@users.noreply.github.com>
2024-02-27 23:56:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const reinitDb = require('./db/reinitDb');
 
 const authRouter = require('./router/authRouter');
 const userRouter = require('./router/userRouter');
-//const adminRouter = require('./router/adminRouter');
+const adminRouter = require('./router/adminRouter');
 const intelRouter = require('./router/intelRouter');
 //const testRouter = require('./router/testRouter');
 
@@ -26,7 +26,7 @@ server.disable('x-powered-by');
 
 server.use(appConfig.AUTH_URL_PREFIX, authRouter);
 server.use(appConfig.USER_URL_PREFIX, userRouter);
-//server.use(appConfig.ADMIN_URL_PREFIX, adminRouter);
+server.use(appConfig.ADMIN_URL_PREFIX, adminRouter);
 server.use(appConfig.INTEL_URL_PREFIX, intelRouter);
 //server.use('/api/testRoute',testRouter);
 
