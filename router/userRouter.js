cc21e79b78a6ddd2983006fcbedc87e316253f4f
Abhinav R <76396917+Abhinav-ark@users.noreply.github.com>
2024-03-02 11:57:50 +0530

    production push

--- router/userRouter.js
+++ router/userRouter.js
@@ -5,16 +5,16 @@ const userRouter = express.Router();
 
 userRouter.get('/test', userController.testConnection);
 userRouter.get('/getAllEvents', userController.getAllEvents);
-userRouter.get('/getRegisteredEvents', userController.getRegisteredEvents);
+//userRouter.get('/getRegisteredEvents', userController.getRegisteredEvents);
 userRouter.get('/getStarredEvents', userController.getStarredEvents);
 userRouter.post('/toggleStarredEvent', userController.toggleStarredEvent);
 userRouter.get('/getStudentProfile', userController.getStudentProfile);
 userRouter.post('/editStudentProfile', userController.editStudentProfile);
-userRouter.post('/buyPassport',userController.buyPassport);
-userRouter.post('/registeredEventData', userController.registeredEventData);
+//userRouter.post('/buyPassport',userController.buyPassport);
+//userRouter.post('/registeredEventData', userController.registeredEventData);
 userRouter.get('/getEventData/:eventId(\\d+)', userController.getEventData);
-userRouter.post('/registerForEventStepOne', userController.registerForEventStepOne);
-userRouter.post('/verifyTransaction', userController.verifyTransactionPayU);
-userRouter.get('/getAllTransactions', userController.getAllTransactions);
+//userRouter.post('/registerForEventStepOne', userController.registerForEventStepOne);
+//userRouter.post('/verifyTransaction', userController.verifyTransactionPayU);
+//userRouter.get('/getAllTransactions', userController.getAllTransactions);
 
 module.exports = userRouter;
