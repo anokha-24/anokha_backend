dde8bb37dbab10700640fae23bb59b6daa5ad595
Abhinav R <76396917+Abhinav-ark@users.noreply.github.com>
2024-03-07 13:48:45 +0530

    passport price updated
    Co-authored-by: Ashwin <ashrockzzz2003@gmail.com>

--- controller/userController.js
+++ controller/userController.js
@@ -2161,7 +2161,7 @@ module.exports = {
                 }
 
                 const txnId = `TXN-P-${req.body.studentId.toString()}-${new Date().getTime()}`;
-                let amount = 500;
+                let amount = 300;
                 let productinfo = `P-${req.body.studentId.toString()}`;
                 let firstname = studentData[0].studentFullName;
                 let email = studentData[0].studentEmail;
