717da5646b8364dfe0b95c90120bb3d30a3baa50
Abhinav R <76396917+Abhinav-ark@users.noreply.github.com>
2024-10-06 10:40:40 +0530

    getEventRegistrationData - Changes as of now.

--- db/sql/adminController/queries.js
+++ db/sql/adminController/queries.js
@@ -1,5 +1,7 @@
 const unlockTables = {
-  queries: ["UNLOCK TABLES"],
+  queries: {
+    unlock:"UNLOCK TABLES"
+  },
 };
 
 const getAdminProfile = {
@@ -35,7 +37,92 @@ const getEventRegistrationCount = {
     }
 };
 
-module.exports = { unlockTables, 
+
+// const jsonSchema = `
+// JSON_OBJECT(
+//     'eventId', e.eventId,
+//     'isGroup', e.isGroup,
+//     'registrations', (
+//         SELECT JSON_ARRAYAGG(
+//             JSON_OBJECT(
+//                 'registrationId', erd.registrationId,
+//                 'txnID', erd.txnId,
+//                 'teamName', erd.teamName,
+//                 'teamData', (
+//                     SELECT JSON_ARRAYAGG(
+//                         JSON_OBJECT(
+//                             'studentId', COALESCE(ergd.studentId, erd.studentId),
+//                             'role', ergd.roleDescription,
+//                             'studentFullName', sd.studentFullName,
+//                             'studentCollegeName', sd.studentCollegeName,
+//                             'studentCollegeCity', sd.studentCollegeCity,
+//                             'studentEmail', sd.studentEmail,
+//                             'studentPhone', sd.studentPhone
+//                         )
+//                     )
+//                     FROM (
+//                         SELECT erd2.registrationId, erd2.studentId, NULL as roleDescription
+//                         FROM eventRegistrationData erd2
+//                         WHERE erd2.eventId = e.eventId AND e.isGroup = '0'
+//                         UNION ALL
+//                         SELECT ergd2.registrationId, ergd2.studentId, ergd2.roleDescription
+//                         FROM eventRegistrationGroupData ergd2
+//                         WHERE ergd2.eventId = e.eventId AND e.isGroup = '1'
+//                     ) ergd
+//                     JOIN studentData sd ON sd.studentId = ergd.studentId
+//                     WHERE ergd.registrationId = erd.registrationId
+//                 )
+//             )
+//         )
+//         FROM eventRegistrationData erd
+//         WHERE erd.eventId = e.eventId
+//     )
+// ) 
+// AS result`
+
+const getEventRegistrationData = {
+    locks: {
+      lockEventData_eventRegistrationData_eventRegistrationGroupData_studentData: 
+      `
+      LOCK TABLES 
+      eventData READ, 
+      eventRegistrationData READ, 
+      eventRegistrationGroupData READ,
+      studentData READ
+      `
+    },
+    queries: {
+      getEventData: 
+      `SELECT 
+      eventId,
+      eventName,
+      eventDate,
+      eventTime,
+      eventVenue,
+      eventPrice,
+      maxSeats,
+      seatsFilled,
+      minTeamSize,
+      maxTeamSize,
+      isWorkshop,
+      isTechnical,
+      isGroup,
+      isPerHeadPrice,
+      eventStatus,
+      eventDepartmentId
+      FROM eventData 
+      WHERE eventId = ?`,
+      getAllEventRegistrationData:`
+
+      `,
+      getDepartmentEventRegistrationData: ``,
+      getSpecificEventRegistrationData: ``
+    }
+};
+
+module.exports = { 
+  unlockTables, 
   getAdminProfile,
-  getEventRegistrationCount
+  getEventRegistrationCount,
+  getEventRegistrationData
 };
